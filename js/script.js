59f4d18705cdf84fd26043d24a0a4ece69d2a252
Rokan Uddin <rokanu55@gmail.com>
2021-08-07 15:07:06 +0600

    photo gallary added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 function showAus() {
     document.getElementsByClassName("aus-players")[0].style.display = "flex";
     document.getElementsByClassName("bd-players")[0].style.display = "none";
+    document.getElementsByClassName("aus-btn")[0].style.backgroundColor = "rgba(128, 128, 128, 0.15)";
+    document.getElementsByClassName("bd-btn")[0].style.backgroundColor = "white";
+    document.getElementsByClassName("aus-btn")[0].style.fontWeight = "bold";
+    document.getElementsByClassName("bd-btn")[0].style.fontWeight = "normal";
 }
 function showBd() {
     document.getElementsByClassName("bd-players")[0].style.display = "flex";
     document.getElementsByClassName("aus-players")[0].style.display = "none";
+    document.getElementsByClassName("bd-btn")[0].style.backgroundColor = "rgba(128, 128, 128, 0.15)";
+    document.getElementsByClassName("aus-btn")[0].style.backgroundColor = "white";
+    document.getElementsByClassName("bd-btn")[0].style.fontWeight = "bold";
+    document.getElementsByClassName("aus-btn")[0].style.fontWeight = "normal";
+
 }
