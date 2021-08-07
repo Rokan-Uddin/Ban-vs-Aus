d8b1ae8ee83ce79d61e4121f6f8de53d2c8d6a30
Rokan Uddin <rokanu55@gmail.com>
2021-08-07 08:10:52 +0600

    Squads section added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showAus() {
+    document.getElementsByClassName("aus-players")[0].style.display = "flex";
+    document.getElementsByClassName("bd-players")[0].style.display = "none";
+}
+function showBd() {
+    document.getElementsByClassName("bd-players")[0].style.display = "flex";
+    document.getElementsByClassName("aus-players")[0].style.display = "none";
+}
